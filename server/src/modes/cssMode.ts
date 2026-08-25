c6d08b43becf7612896a05f305c1adedd7b05103
Austin Simmons <austin_simmons@fastmail.com>
2026-08-25 06:56:00 -0700

    Disable Emmet completion in CSS/style regions

--- server/src/modes/cssMode.ts
+++ server/src/modes/cssMode.ts
@@ -1,8 +1,6 @@
 import { getCSSLanguageService } from 'vscode-css-languageservice';
-import { CompletionList } from 'vscode-languageserver/node';
 import { LanguageMode } from '../languageModes';
 import { getEmbeddedDocument } from '../documentRegions';
-import { getEmmetCompletion } from './emmet';
 
 const cssLanguageService = getCSSLanguageService();
 
@@ -12,13 +10,7 @@ export function getCSSMode(): LanguageMode {
     doComplete(document, position, regions) {
       const embedded = getEmbeddedDocument(document, regions, 'css');
       const stylesheet = cssLanguageService.parseStylesheet(embedded);
-      const result = cssLanguageService.doComplete(embedded, position, stylesheet);
-      const emmet = getEmmetCompletion(embedded, position, 'css');
-      if (!emmet) return result;
-      return CompletionList.create(
-        [...result.items, ...emmet.items],
-        result.isIncomplete || emmet.isIncomplete,
-      );
+      return cssLanguageService.doComplete(embedded, position, stylesheet);
     },
     doDiagnostics(document, regions) {
       const embedded = getEmbeddedDocument(document, regions, 'css');
